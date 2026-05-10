6bedb7907bf351244ab6453727a6c76e0756c1cf
omahr <omahrous112@gmail.com>
2026-05-10 16:22:12 +0300

    Initial commit

---
+++ src/ExternalIdValidator.java
@@ -0,0 +1,9 @@
+import java.util.Objects;
+
+public class ExternalIdValidator {
+
+    public static boolean compareExternalIds(String userId, String instructorId) {
+        return Objects.equals(userId, instructorId);
+    }
+
+}
---
+++ src/ExternalIdValidatorTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExternalIdValidatorTest {
+
+    @Test
+    public void testNullComparison() {
+        assertFalse(
+                ExternalIdValidator.compareExternalIds(null, "INS123")
+        );
+    }
+
+    @Test
+    public void testEqualIds() {
+        assertTrue(
+                ExternalIdValidator.compareExternalIds("INS123", "INS123")
+        );
+    }
+
+    @Test
+    public void testDifferentIds() {
+        assertFalse(
+                ExternalIdValidator.compareExternalIds("INS123", "INS999")
+        );
+    }
+}
